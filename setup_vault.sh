0b032c2bc016846cf45f83c541837c8916b8c29e
Radoslaw.Wegrzyk <radoslaw.wegrzyk@britenet.com.pl>
2023-08-22 20:21:52 +0200

    add new vault for ready project v1.0

--- setup_vault.sh
+++ setup_vault.sh
@@ -1,6 +1,7 @@
 VAULT_ADDR="http://vault:8200"
 VAULT_TOKEN="token"
 file="./secrets.env"
+file_postgres="./secrets_postgres.env"
 
 apk --no-cache add curl
 apk --no-cache add jq
@@ -9,7 +10,7 @@ apk --no-cache add jq
 
 curl --request POST \
   --header "X-Vault-Token: $VAULT_TOKEN" \
-  --data '{"data":{"user":"user","password":"password"}}' \
+  --data '{"data":{"user":"user","password":"password","postgresuser":"airflow","postgrespassword":"airflow","postgresdb":"airflow"}}' \
   "$VAULT_ADDR/v1/secret/data/airflow_secret"
 
 
@@ -18,11 +19,19 @@ VAULT_DATA=$(curl --request GET --header "X-Vault-Token: $VAULT_TOKEN" "$VAULT_A
 
 USER=$(echo "$VAULT_DATA" | jq -r '.data.data.user')
 PASSWORD=$(echo "$VAULT_DATA" | jq -r '.data.data.password')
+POSTGRES_USER=$(echo "$VAULT_DATA" | jq -r '.data.data.postgresuser')
+POSTGRES_PASSWORD=$(echo "$VAULT_DATA" | jq -r '.data.data.postgrespassword')
+POSTGRES_DB=$(echo "$VAULT_DATA" | jq -r '.data.data.postgresdb')
 
 
 echo "_AIRFLOW_WWW_USER_USERNAME=$USER"
 echo "_AIRFLOW_WWW_USER_PASSWORD=$PASSWORD"
-
+echo "POSTGRES_USER=$POSTGRES_USER"
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD"
+echo "POSTGRES_DB=$POSTGRES_DB"
 
 echo "_AIRFLOW_WWW_USER_USERNAME=$USER" > $file
 echo "_AIRFLOW_WWW_USER_PASSWORD=$PASSWORD" >> $file
+echo "POSTGRES_USER=$POSTGRES_USER" > $file_postgres
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $file_postgres
+echo "POSTGRES_DB=$POSTGRES_DB" >> $file_postgres
